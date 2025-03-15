f14955369f80d8b4a8aa1f9c0445b47a9da3486f
gabriel barbosa <gabrielturing@gmail.com>
2025-03-15 13:39:25 -0300

    feat: add logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "github.com/gabrielbsx/opportunities-api/router"
+import (
+	"github.com/gabrielbsx/opportunities-api/config"
+	"github.com/gabrielbsx/opportunities-api/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("Opportunities API")
+
+	// Initialize configs
+	err := config.Init()
+
+	if err != nil {
+		logger.Error("Error initializing configs: ", err)
+		return
+	}
+
 	// Initialize routers
 	router.Initialize()
 }
