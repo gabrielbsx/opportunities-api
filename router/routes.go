5a56bf836fac9e7cec100138e34cd90f79662d91
gabriel barbosa <gabrielturing@gmail.com>
2025-03-15 13:12:51 -0300

    refactor: remove ping route and add group version route

--- router/routes.go
+++ router/routes.go
@@ -3,10 +3,12 @@ package router
 import "github.com/gin-gonic/gin"
 
 func initializeRoutes(router *gin.Engine) {
-	// Ping test
-	router.GET("/ping", func(context *gin.Context) {
-		context.JSON(200, gin.H{
-			"message": "pong",
+	v1 := router.Group("/api/v1")
+	{
+		v1.GET("/opening", func(context *gin.Context) {
+			context.JSON(200, gin.H{
+				"message": "Opening endpoint",
+			})
 		})
-	})
+	}
 }
